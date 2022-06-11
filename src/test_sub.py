c749dd400b93fc753c45c63cc2a2e694a556c96b
Williamiamiam <59093102+WilliamR312@users.noreply.github.com>
2022-06-11 11:18:02 +0200

    Update test_sub.py

--- src/test_sub.py
+++ src/test_sub.py
@@ -4,12 +4,11 @@ class TestsSub():
 
     def test_sub(self):
         assert sub(1, 1) == 0
-        assert sub (4, 5) == 9 # Supposed to fail
+        assert sub (4, 5) == -1
 
     def test_sub_int(self):
         assert sub(1, 2) == -1
         assert sub(1, 1) == 0
-
 
     def test_sub_negative(self):
         assert sub(1, -1) == 2
